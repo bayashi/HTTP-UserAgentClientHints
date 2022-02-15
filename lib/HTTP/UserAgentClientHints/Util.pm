1215d91da4ceef92f941d82b5296cfc5e1cda536
bayashi <bayashi@cpan.org>
2022-02-15 11:05:00 +0000

    fix

--- lib/HTTP/UserAgentClientHints/Util.pm
+++ lib/HTTP/UserAgentClientHints/Util.pm
@@ -14,6 +14,8 @@ sub strip_quote {
     return $value;
 }
 
+1;
+
 __END__
 
 =encoding UTF-8
@@ -37,6 +39,3 @@ Dai Okabayashi E<lt>bayashi@cpan.orgE<gt>
 C<HTTP::UserAgentClientHints::Util> is free software; you can redistribute it and/or modify it under the terms of the Artistic License 2.0. (Note that, unlike the Artistic License 1.0, version 2.0 is GPL compatible by itself, hence there is no benefit to having an Artistic 2.0 / GPL disjunction.) See the file LICENSE for details.
 
 =cut
-
-
-1;
